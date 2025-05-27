c9eda19b1057d0f96aac81ac12ebabc4949161ae
Lalit Sharma <ls8290519977@gmail.com>
2025-05-27 20:03:47 +0530

    added page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import Hero from "@/components/home/Hero";
+import Highlights from "@/components/home/Highlights";
+import KeynoteSpeakers from "@/components/home/KeynoteSpeakers";
+import Overview from "@/components/home/Overview";
+import Tracks from "@/components/home/Tracks";
+import ImportantDates from "@/components/ImportantDates";
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Overview />
+            <Tracks />
+            <ImportantDates />
+            <KeynoteSpeakers />
+            <Highlights />
+        </>
+    )
+}
+
+export default Home;
