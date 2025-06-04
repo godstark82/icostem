52fc23252aa21d0f7d0febb131d8ddffd61d0db4
Lalit Sharma <ls8290519977@gmail.com>
2025-06-04 22:49:43 +0530

    test

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const db = getFirestore(app);
